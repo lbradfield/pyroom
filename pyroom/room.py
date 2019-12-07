616ba9c9a2e386ea2f6f921ec0a934b8691507d9
Louis Carleton <louis.carleton@gmail.com>
2019-12-07 14:57:21 -0800

    added module init file; wrote function to get walls from points; updated add_wall function to only add if wall doesn't exist

--- pyroom/room.py
+++ pyroom/room.py
@@ -6,6 +6,8 @@
 import sys
 import logging
 #import sympy
+#from euclid import *
+import euclid
 
 import test
 
@@ -39,23 +41,106 @@ class Room:
 
     def __init__(self):
         logger.debug("Func() : " + sys._getframe().f_code.co_name)
-        self.origin = (0,0)
+        self.points = []
+        self.walls = []
+        self.width = None
+        self.depth = None
+
+    # TODO: Impliment a room generation based on input width and
+    # depth
+    # def set_size(self, width, depth):
+    #     logger.debug("Func() : " + sys._getframe().f_code.co_name)
+    #     self.width = width
+    #     self.depth = depth
 
     def get_size(self):
-        return self.width, self.depth
+        return self.width * self.depth
 
     def get_name(self):
         return self.name
+
+    def get_walls(self):
+        return self.walls
+
+    def get_points(self):
+        return self.points
 
     def set_name(self, name):
         logger.debug("Func() : " + sys._getframe().f_code.co_name)
         self.name = name
 
-    def set_size(self, width, depth):
+    def add_point(self, x, y):
         logger.debug("Func() : " + sys._getframe().f_code.co_name)
-        self.width = width
-        self.depth = depth
+        point = euclid.Point2(x, y)
+        if point not in self.points:
+            self.points.append(point)
+
+    # TODO: Add capability to create diagonal walls
+    def find_walls(self):
+        # Connect points to form walls if the points have either the
+        # same X coordinate or the same Y coordinate
+        logger.debug("Func() : " + sys._getframe().f_code.co_name)
+        points_len = len(self.points)
+        for i in range(points_len):
+            # Compare this point to the rest of the points in the
+            # list
+            #logger.debug("for {}".format(self.points[i]))
+            for j in range(i + 1, points_len):
+                #logger.debug("against {}".format(self.points[j]))
+                # If the X coords match or the Y coords match
+                if self.points[i].x == self.points[j].x or \
+                self.points[i].y == self.points[j].y:
+                    # Create a wall from these two points
+                    self.add_wall(self.points[i], self.points[j])
+
+    def add_wall(self, point1, point2):
+        # Input 2 euclid.Points and create a wall to add to
+        # self.walls
+        logger.debug("Func() : " + sys._getframe().f_code.co_name)
+        if self.walls != []:
+            for wall in self.walls:
+                # If both points in input already comprise an existing
+                # wall
+                if point1 in wall and point2 in wall:
+                    logger.warning("Wall already exists")
+                    return
+        logger.debug("Adding wall: {}".format((point1,
+                                               point2)))
+        self.walls.append((point1, point2))
+
+    def get_rectangle_area(self, x_len, y_len):
+        return x_len * y_len
+
+    def get_room_area(self):
+        pass
+
+    def get_slope(self, point1, point2):
+        # Find the slope between two points
+        logger.debug("Func() : " + sys._getframe().f_code.co_name)
+        numerator = 
+
+# TODO:
+    # 1. Write funct to get rid of wall segments that are in union
+    # 2. Write funct to save areas of wall boxes before wall segments
+    # are removed
 
 
 
-print(test.test_func())
+if __name__ == "__main__":
+    room = Room()
+    room.set_name("Living Room")
+    room.add_point(3.0, 0.0)
+    room.add_point(1, 2)
+    room.add_point(1, 5)
+    room.add_point(1, -1)
+    room.add_point(1, 0)
+    room.add_point(0.0, 0.0)
+    room.add_point(3.0, 5.0)
+    room.add_point(0.0, 2.0)
+
+    logger.info("Name: {}".format(room.get_name()))
+    logger.info("Points: {}".format(room.get_points()))
+    room.find_walls()
+    logger.info("Wall Coords: {}".format(room.get_walls()))
+    #logger.info("Size: {} sq ft".format(room.get_size()))
+
