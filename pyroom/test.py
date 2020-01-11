899c1d7c11222d10bd382d32d0171574b3f29722
Louis Carleton <louis.carleton@gmail.com>
2020-01-11 15:44:00 -0800

    created basic test cases for room and furniture objects

--- pyroom/test.py
+++ pyroom/test.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
-def test_func():
-    return 1
+############################
+# Imports
+############################
+from room import Room
+from furniture import Furniture
+
+if __name__ == "__main__":
+
+    kitchen_points = [
+        (1, 1),
+        (3, 2),
+        (3, 6),
+        (1, 5),
+    ]
+
+    fridge_points = [
+        (0, 0),
+        (1, 0),
+        (1, 1),
+        (0, 1),
+    ]
+
+    kitchen = Room("Kitchen", kitchen_points)
+    logger.info(kitchen)
+
+    fridge = Furniture("Refrigerator", fridge_points)
+    logger.info(fridge)
