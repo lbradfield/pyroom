a153ded9a7f439ad09e1684a084168a278999947
Louis Carleton <louis.carleton@gmail.com>
2020-01-11 15:28:55 -0800

    created config file with logging constants

---
+++ pyroom/config.py
@@ -0,0 +1,17 @@
+############################
+# Imports
+############################
+import sys
+import logging
+#import sympy
+#from euclid import *
+#import euclid
+#import numpy as np
+
+############################
+# Logging
+############################
+LOG_LEVEL = logging.DEBUG
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+
