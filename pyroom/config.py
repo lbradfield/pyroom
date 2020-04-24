c5fd1fb9edb29c21d51da7f60ee9dc0530398fa0
Louis Carleton <louis.carleton@gmail.com>
2020-04-23 23:46:25 -0700

    adding config file with logging setup

---
+++ pyroom/config.py
@@ -0,0 +1,24 @@
+############################
+# Imports
+############################
+import logging
+
+############################
+# Logging setup
+############################
+LOG_LEVEL = logging.DEBUG
+LOG_FORMAT = \
+    "%(asctime)s|%(module)s|%(funcName)s|%(levelname)s|%(message)s"
+LOG_NAME = "pyroom"
+# Create the formatter
+formatter = logging.Formatter(LOG_FORMAT)
+# Create the console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(LOG_LEVEL)
+console_handler.setFormatter(formatter)
+# Create the logger
+logger = logging.getLogger(LOG_NAME)
+logger.setLevel(LOG_LEVEL)
+# Add handlers to the logger
+logger.addHandler(console_handler)
+
