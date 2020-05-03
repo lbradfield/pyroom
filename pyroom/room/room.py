10d97d3593eb74ea0f5aec5fd6a720916b6d134f
Louis Carleton <louis.carleton@gmail.com>
2020-05-02 20:29:02 -0700

    consolidated similar objects into one module

--- pyroom/room/room.py
+++ pyroom/room/room.py
@@ -3,28 +3,111 @@
 ############################
 # Imports
 ############################
-from polygon import Polygon
+# Standard imports
+import sys
 from config import *
 
-############################
-# Set up logging
-############################
-LOG_NAME = __name__
-# Create the formatter
-formatter = logging.Formatter(LOG_FORMAT)
-# Create the logger
-logger = logging.getLogger(LOG_NAME)
-logger.setLevel(LOG_LEVEL)
-# Create the console handler
-console_handler = logging.StreamHandler()
-console_handler.setLevel(LOG_LEVEL)
-console_handler.setFormatter(formatter)
-# Add handlers to the logger
-logger.addHandler(console_handler)
 
-############################
-# Object
-############################
+class Polygon:
+
+    def __init__(self, points):
+        '''
+        Construct with an ordered list of 2-element tuples of
+        integers, which represent points, from which to draw the
+        vertices of the polygon.
+
+        Parameters
+        ----------
+        points : list
+            An ordered list of 2-element tuples of integers
+            representing points in the polygon, not including the
+            origin (0, 0).
+        '''
+        self.vertices = [(0, 0)]
+        self.vertices.extend(points)
+        self.num_vertices = len(self.vertices)
+
+        # Create list of segments
+        #self.set_segments(points)
+
+    def __str__(self):
+        text = "Polygon with vertices:"
+        for point in self.vertices:
+            text += "\n{}".format(point)
+        return text
+
+    def calc_area(self):
+        '''
+        Calculate the area using the Shoelace Formula.
+        '''
+        area = 0.0
+        for i in range(self.num_vertices):
+            j = (i + 1) % self.num_vertices
+            area += self.vertices[i][0] * self.vertices[j][1]
+            area -= self.vertices[j][0] * self.vertices[i][1]
+        self.area = abs(area) * 0.5
+
+    # Unused functions below
+    # ----------------------
+    def rotate(self, deg):
+        '''
+        Rotate the polygon about the origin.
+        '''
+
+    def get_vertices(self):
+        return self.vertices
+
+    def get_area(self):
+        return self.area
+
+    def set_segments(self, points):
+        '''
+        Incomplete and not needed at this time.
+        Input segments as an ordered list of points, and output a
+        list of lists of tuples, clumping together vertices that are
+        adjacent.
+
+        Parameters
+        ----------
+        points : 
+
+        Examples
+        --------
+            [(0, 1), (0, 4), (
+        '''
+        pass
+
+    def add_point(self, x, y):
+        '''
+        Incomplete and not required at this time
+        '''
+        point = euclid.Point2(x, y)
+        if point not in self.points:
+            self.points.append(point)
+
+
+class Furniture(Polygon):
+    '''
+    Furniture object that interacts with room object.
+    '''
+
+    # Get units from config
+    units = UNITS
+
+    def __init__(self, name, points):
+        self.name = name
+        Polygon.__init__(points)
+
+    def __str__(self):
+        return "{} - {} {}".format(self.name, self.area, self.units)
+
+    def set_wallside(self, seg):
+        '''
+        Designates a particular segment to be wall-facing.
+        '''
+        self.wallside = seg
+
+
 class Room(Polygon):
     '''
     Room object which holds furniture and inherits basic polygon
@@ -58,6 +141,5 @@ class Room(Polygon):
         furn : object
         '''
         self.furniture.append((furn, pos))
-
 
 
