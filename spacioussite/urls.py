cdcf8d7bbc6f1bd8f79d6a24ff33edf35248fa21
Louis Carleton <louis.carleton@gmail.com>
2021-01-21 21:39:54 -0800

    moved django code up one level

--- spacioussite/urls.py
+++ spacioussite/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from . import views
+from django.views.generic.base import TemplateView
+import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('polls/', include('polls.urls')),
 ]
