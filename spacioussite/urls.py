d6508ed105618264d70549335c9ef0dddb2a2143
Louis Carleton <louis.carleton@gmail.com>
2021-01-31 18:59:29 -0800

    reformatted files for cleanliness. Added profile page button that greys out if on the profile url

--- spacioussite/urls.py
+++ spacioussite/urls.py
@@ -19,12 +19,17 @@ from django.contrib.auth import views as auth_views
 import views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
-    path('', views.index, name='home'),
-    path('about', views.about, name='about'),
-    path('contact', views.contact, name='contact'),
-    
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+    path("", views.index, name="home"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("accounts/profile", views.ProfileView.as_view(), name="profile"),
     # Django auth stuff
-    path('accounts/login', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login')
+    path(
+        "accounts/login",
+        auth_views.LoginView.as_view(template_name="accounts/login.html"),
+        name="login",
+    ),
+    path("accounts/logout", auth_views.LogoutView.as_view(), name="logout"),
 ]
