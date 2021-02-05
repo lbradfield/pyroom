9d28c11f2d1fa7c4d13477fe5ec462b81a88f69b
Louis Carleton <louis.carleton@gmail.com>
2021-02-04 21:35:46 -0800

    added accounts app urls

--- spacioussite/apps/accounts/urls.py
+++ spacioussite/apps/accounts/urls.py
@@ -4,12 +4,12 @@ import apps.accounts.views
 
 app_name = "accounts"
 urlpatterns = [
-    path("accounts/profile", apps.accounts.views.ProfileView.as_view(), name="profile"),
+    path("profile", apps.accounts.views.ProfileView.as_view(), name="profile"),
     # Django auth stuff
     path(
-        "accounts/login",
+        "login",
         auth_views.LoginView.as_view(template_name="accounts/login.html"),
         name="login",
     ),
-    path("accounts/logout", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout", auth_views.LogoutView.as_view(), name="logout"),
 ]
