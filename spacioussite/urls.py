52d23021c7051737e8d95f591d87547ca0e11711
Louis Carleton <louis.carleton@gmail.com>
2021-02-11 21:38:54 -0800

    moved polls app under apps

--- spacioussite/urls.py
+++ spacioussite/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.public.urls")),
     path("accounts/", include("apps.accounts.urls")),
-    path("polls/", include("polls.urls")),
+    path("polls/", include("apps.polls.urls")),
 ]
