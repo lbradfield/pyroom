bc12883a228d035aa5ac75ce07317c9eb817c5a5
Louis Carleton <louis.carleton@gmail.com>
2021-02-11 21:43:14 -0800

    added basic accounts index view

--- spacioussite/apps/accounts/views.py
+++ spacioussite/apps/accounts/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+def index(request):
+    return HttpResponse("Hello world, you're at the accounts index!")
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = "accounts/profile.html"
