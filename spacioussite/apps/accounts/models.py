cf14fb01a64052c53213091eb55dcb98676f8455
Louis Carleton <louis.carleton@gmail.com>
2021-05-10 19:00:52 -0700

    old commits that I missed

--- spacioussite/apps/accounts/models.py
+++ spacioussite/apps/accounts/models.py
@@ -13,10 +13,9 @@ class UserPersona(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    
     is_full_name_displayed = models.BooleanField(default=True)
 
     # details
-    persona = models.ForeignKey(UserPersona, on_delete=models.SET_NULL, blank=True, null=True)
     bio = models.CharField(max_length=500, blank=True, null=True)
     website = models.URLField(max_length=200, blank=True, null=True)
+    persona = models.ForeignKey(UserPersona, on_delete=models.SET_NULL, blank=True, null=True)
